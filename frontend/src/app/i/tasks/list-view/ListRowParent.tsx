b25a1ebd7ae4d2cd5d66cb37d05d2cec3b8cdbfd
ilzat <ilzat.sadykov.2006@bk.ru>
2025-02-07 03:20:34 +0600

    'fix bug with more request '

--- frontend/src/app/i/tasks/list-view/ListRowParent.tsx
+++ frontend/src/app/i/tasks/list-view/ListRowParent.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import { Draggable, Droppable } from '@hello-pangea/dnd'
 import type { Dispatch, SetStateAction } from 'react'
 import { ListAddRowInput } from './ListAddRowInput'
 import styles from './ListView.module.scss'
-import { filterTasks } from '@/app/i/tasks/filter-task'
 import { FILTERS } from '@/app/i/tasks/columns.data'
+import { filterTasks } from '@/app/i/tasks/filter-task'
 import { ListRow } from '@/app/i/tasks/list-view/ListRow'
 import { ITaskResponse } from '@/types/task.types'
 
@@ -30,27 +32,30 @@ export function ListRowParent({
 					<div className={styles.colHeading}>
 						<div className={'w-full'}>{label}</div>
 					</div>
-					{filterTasks(items, value)?.map((item, index) => (
-						<Draggable
-							key={item.id}
-							draggableId={String(item.id)}
-							index={index}
-						>
-							{provided => (
-								<div
-									ref={provided.innerRef}
-									{...provided.draggableProps}
-									{...provided.dragHandleProps}
-								>
-									<ListRow
-										key={item.id}
-										item={item}
-										setItems={setItems}
-									/>
-								</div>
-							)}
-						</Draggable>
-					))}
+					{filterTasks(items, value)?.map((item, index) => {
+						const draggableId = item.id ? item.id : `${index}`;
+						return (
+							<Draggable
+								key={item.id}
+								draggableId={draggableId}
+								index={index}
+							>
+								{provided => (
+									<div
+										ref={provided.innerRef}
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+									>
+										<ListRow
+											key={item.id}
+											item={item}
+											setItems={setItems}
+										/>
+									</div>
+								)}
+							</Draggable>
+						)
+					})}
 
 					{provided.placeholder}
 
